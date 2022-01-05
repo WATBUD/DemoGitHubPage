b84a29d072b749090dec3391b0383632f44599fb
louischin <z23320785@gmail.com>
2022-01-05 18:18:09 +0800

    add

--- src/app/perixx/SettingManager.ts
+++ src/app/perixx/SettingManager.ts
@@ -2,11 +2,6 @@
 export class SettingManager {
     deviceModel="Periboard-XXX";
     battery="Periboard-XXX";
-    layoutMode="Default";//Default Custom
-    brightness=100;
-    macroEnable=true;
-    autoSleepEnable=true;
-    autoSleepValue=60;
     constructor(inputmax=1,quantity=0) {
 
     }
